c57beba2d0aeeeba0d24595cd7531fd027a20a51
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-03-30 13:45:40 +0100

    led added

--- led.sh
+++ led.sh
@@ -1,2 +1,7 @@
-git clone https://github.com/jgarff/rpi_ws281x.git
+# https://core-electronics.com.au/tutorials/ws2812-addressable-leds-raspberry-pi-quickstart-guide.html
+# https://www.youtube.com/watch?v=Pxt9sGTsvFk
+
+# git clone https://github.com/jgarff/rpi_ws281x.git
+
+curl -L http://coreelec.io/33 | bash
 
