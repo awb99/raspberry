15354f0e9b73cb0669196d9dbc164c772038e45e
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-04-01 20:50:36 +0200

    python3 added

--- voiceDetection.py
+++ voiceDetection.py
@@ -1,5 +1,8 @@
+# snowboy:
 # https://snowboy.kitt.ai/
 # http://docs.kitt.ai/snowboy/
 # https://www.youtube.com/watch?v=6rjXNUFIwzg
 # https://pimylifeup.com/raspberry-pi-snowboy/
 
+# sopare:
+# https://github.com/bishoph/sopare
